341e31d7b8b33dc3257dc6d4831a3302173f18bc
Sharat Masetty <sharat_masetty@yahoo.com>
2017-03-19 19:55:01 +0530

    Connecting more dots.
    This patch paves way for connecting the frontend server with the API
    server. This will happen in the next patch-set.
    
    Playing with a little styling.
    Setup grunt taks for the real grunt work.
    getting more comfortable with pug styling.
    and yes livereload is really awesome.

--- coupon-frontend/app/js/script.js
+++ coupon-frontend/app/js/script.js
@@ -1,12 +1,44 @@
 var modal = document.getElementById('js-success');
 var form = document.getElementById('js-signup-form');
-
+ 
 window.onclick = function(event) {
   if(event.target === modal) modal.style.display = 'none';
 }
 
 function submitForm() {
-  console.log(form);
+  displayMessage('');
+  var errorMessage = false;
+
+  if (!validatePhone(form.phone, true)) {
+    error(form.phone);
+    errorMessage = 'Missing/Invalid Phone Number'
+  }
+  
+  if (!validateEmail(form.email, false)){
+    error(form.email);
+    errorMessage += '<br /> Missing Email'
+  }
+    
+  if(errorMessage) {
+    displayMessage(errorMessage);
+    return;
+  } 
+
+  var data = {
+    phone: form.phone.value,
+    phoneProvider: form.phoneProvider.value
+  };
+
+  fetch('/', {
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    method: 'POST',
+    body: JSON.stringify(data)
+  }).then(submitSuccess)
+  .catch(submitError);
+
+  console.log("Done submitting the form to the API server");
 }
 
 function error(target){
@@ -15,9 +47,38 @@ function error(target){
 }
 
 function clearError(target){
-
+  target.style.border=''
 }
 
+function clearForm() {
+  form.reset();
+  clearError(form.js-error-message);
+  var divs = document.getElementsByClassName('hidden');
+  for (var i = 0; i < divs.length; i++)
+    divs[i].style.display ='';
+}
+
+function submitSuccess(res) {
+  if(!res.ok) return submitError(res);
+  modal.style.display = 'block';
+  clearForm();
+}
+
+function submitError(res, message) {
+  if (res.status >= 400 && res.status <= 500)
+    //return res.text().then(function(message) {displayMessage(message)});
+     //res.text().then(function(message) {displayMessage(message)});
+    res.text();
+  if (message)
+    return displayMessage(message);
+  return displayMessage('There was an error with the form, please try again later.');
+}
+
+function displayMessage(message){
+  var errorDiv = document.getElementById('js-error-message');
+  errorDiv.innerHTML = message;
+  errorDiv.style.visibility = 'visible';
+}
 
 function validatePhone(target, isRequired) {
   var phone = target.value;
@@ -43,4 +104,30 @@ function validateEmail(target, isRequired){
   return isValid;
 }
 
+function validateProvider(isRequired) {
+  var target = form.phoneProvider;
+  clearError(target);
+  if(target.value === 'other'){
+    if (form['other-provider'].style.display === 'none'){
+      form['other-provider'].style.display = 'inline-block';
+      return false;
+    }
+    else {
+      if (!form['other-provider'].value) {
+        error(form['other-provider']);
+        return false;
+      }
+      else
+        return true;
+    }
+  }
+  else {
+    form['other-provider'].style.display = 'none';
+    if (target.value === 'null' && isRequired) {
+      error(target);
+      return false;
+    }
+    return true;
+  }
+}
 
