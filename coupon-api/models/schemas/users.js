4ce378704be37a3413387f7ce9178c8b4189c569
Sharat Masetty <sharat04@gmail.com>
2017-03-25 23:27:37 +0530

    Fixing the backed User Schema
    Adding logic to hash the password and also a new routine to compare the
    user provided password with hash support.
    Add more prehook save logic to do some much required error checking.

--- coupon-api/models/schemas/users.js
+++ coupon-api/models/schemas/users.js
@@ -1,19 +1,20 @@
 var mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 var Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
 var  userSchema =  new Schema({
   firstName: {type: String, trim: true},
   lastName: {type: String, trim: true},
-  classYear: Number,
+  classYear: {type: Number},
   email: {type: String, unique: true, sparse: true, trim: true},
-  phone: {type: String, unique: true, sparse: true, trim: true},
+  phone: {type: String, unique: true, sparse: true},
   interests: [Number],
   isAdmin: {type: Boolean, index: true},
   isSuperAdmin: {type: Boolean, index: true},
-  hash: String,
+  hash: {type: String},
   companyName: {type: String, trim: true},
-  token: String,
+  token: {type: String},
   },
   {
     toObject: {getters: true},
@@ -25,28 +26,47 @@ var  userSchema =  new Schema({
 ); 
 
 userSchema.pre('save', function(callback){
-  if (this.isAdmin || this.isSuperAdmin) {
-    if (!this.email)
-      return callback(new Error('Missing Email'));
-    if (!this.hash)
-      return callback(new Error('Missing Password'));
-    if (!this.companyName)
-      return callback(new Error('Missing Company Name'));
-  }
-  else {
-    //TODO: Do this part later when working on frontend.
-  }
-  callback();
+    if(this.isAdmin || this.isSuperAdmin) {
+        if (!this.email)
+            return callback(new Error('Missing Email'));
+        if (!this.hash)
+            return callback(new Error('Missing Password'));
+        if (!this.companyName)
+            return  callback(new Error('Missing CompanyName'));
+
+        if (this.isModified('hash'))
+            this.hash = bcrypt.hashSync(this.hash);
+    } else {
+        if (!this.phone)
+            return callback(new Error('Missing Phone'));
+        if (!this.phoneProvider)
+            return callback(new Error('Missing Provide'));
+    }
+
+    if (this.phone) {
+        if (typeof this.phone !== 'string')
+            return callback(new Error('Invalid Phone'));
+        var phone = '';
+        for (var i = 0; i < this.phone.length; i++){
+            if (!isNaN(this.phone[i]))
+                phone += this.phone[i];
+        }
+        if (phone.length !== 10)
+            return callback(new Error('Invalid Phone provided'));
+        this.phone = phone; 
+    }
+
+    callback();
 });
 
-userSchema.methods.greet = function() {
-  console.log('Hi User: ', this.firstName);
+userSchema.methods.comparePassword = function(pw, callback) {
+    bcrypt.compare(pw, this.hash, function(err, isMatch) {
+        if (err) return callback(err);
+        callback(null, isMatch);
+    });
 };
 
-userSchema.methods.checkpw = function(pw) {
-  return (this.hash === pw);
-}
-
 var User = mongoose.model('User', userSchema);
+
 module.exports = User;
 
